a78df454f52769d106c5b78e909ab22ce924b00b
heuzin <matheusqs.arq@gmail.com>
2019-07-26 19:20:15 -0400

    Styled dashboard page

--- conjugator/src/components/Dashboard.js
+++ conjugator/src/components/Dashboard.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import './css/dashboard.css';
 
 class Dashboard extends React.Component {
     render() {
         return (
-            <div>
+            <div className='dashboard-content'>
                 <h3>Dashboard</h3>
 
                 <div>This is dummy data. Sign-up here to gain insights into your progress.</div>
 
-                <div>Present Tense</div>
+                <div className='tense-content'>
+                    <div>Present Tense</div>
 
-                <div>Preterite Tense</div>
+                    <div>Preterite Tense</div>
 
-                <div>Imperfect Tense</div>
+                    <div>Imperfect Tense</div>
+                </div>
+
             </div>
         )
     }
